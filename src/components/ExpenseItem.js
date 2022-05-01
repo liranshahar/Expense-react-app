1a2e370b7067f84ce771ef47d2aeee2d700b357b
Liran Shahar <liranshahar4@gmail.com>
2022-05-01 13:10:31 +0300

    Create app

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,21 @@
+import './ExpenseItem.css'
+
+function ExpenseItem() {
+    return (
+      <>
+      <div class="expense-item">
+          <div>March 28th 2021</div>
+            <div class="expense-item__description">
+              <h2>Car Insurance</h2>
+                <div class = "expense-item__price">$249.67</div>
+
+      </div>          
+      </div>      
+
+      </> 
+      
+       );
+  }
+  
+  export default ExpenseItem;
+  
