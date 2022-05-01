0ae333839d5c272c790d157ebc3c2e2f7179a4d1
Liran Shahar <liranshahar4@gmail.com>
2022-05-01 14:36:46 +0300

    Propsdate update

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,19 @@
+import ExpenseDate from './ExpenseDate'
+
 import './ExpenseItem.css'
 
+
 function ExpenseItem(props) {
+
 
   
     return (
       <>
+
+     
+
       <div class="expense-item">
-          <div>{props.date.toISOString()}</div>
+        <ExpenseDate date= {props.date} />  
             <div class="expense-item__description">
               <h2>{props.title}</h2>
                 <div class = "expense-item__price">${props.amount}</div>
