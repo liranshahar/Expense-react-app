6352f0b3c8358f7f94a390556aab185a6b749e43
Liran Shahar <liranshahar4@gmail.com>
2022-05-01 13:56:18 +0300

    Props update

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,13 +1,15 @@
 import './ExpenseItem.css'
 
-function ExpenseItem() {
+function ExpenseItem(props) {
+
+  
     return (
       <>
       <div class="expense-item">
-          <div>March 28th 2021</div>
+          <div>{props.date.toISOString()}</div>
             <div class="expense-item__description">
-              <h2>Car Insurance</h2>
-                <div class = "expense-item__price">$249.67</div>
+              <h2>{props.title}</h2>
+                <div class = "expense-item__price">${props.amount}</div>
 
       </div>          
       </div>      
